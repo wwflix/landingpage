26f2e165799bef3125c7129e3e30f8e17dbbda75
Nico N. <86585461+wwflix@users.noreply.github.com>
2026-04-08 12:39:13 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-const containerEl = document.querySelector(".container");
+const containerEl=document.querySelector(".container");
+const leftEl=document.querySelector(".left");
+const rightEl=document.querySelector(".right");leftEl.addEventListener("mouseenter",()=>{containerEl.classList.add("active-left")});
+leftEl.addEventListener("mouseleave",()=>{containerEl.classList.remove("active-left")});
+rightEl.addEventListener("mouseenter",()=>{containerEl.classList.add("active-right")});
+rightEl.addEventListener("mouseleave",()=>{containerEl.classList.remove("active-right")});
 
-const leftEl = document.querySelector(".left");
+// const containerEl = document.querySelector(".container");
 
-const rightEl = document.querySelector(".right");
+// const leftEl = document.querySelector(".left");
 
-leftEl.addEventListener("mouseenter", ()=> {containerEl.classList.add("active-left")});
+// const rightEl = document.querySelector(".right");
 
-leftEl.addEventListener("mouseleave", ()=>{containerEl.classList.remove("active-left")});
+// leftEl.addEventListener("mouseenter", ()=> {containerEl.classList.add("active-left")});
 
-rightEl.addEventListener("mouseenter", ()=>{containerEl.classList.add("active-right")});
+// leftEl.addEventListener("mouseleave", ()=>{containerEl.classList.remove("active-left")});
 
-rightEl.addEventListener("mouseleave", ()=>{containerEl.classList.remove("active-right")});
+// rightEl.addEventListener("mouseenter", ()=>{containerEl.classList.add("active-right")});
+
+// rightEl.addEventListener("mouseleave", ()=>{containerEl.classList.remove("active-right")});
 // d3.csv('youtubeVid_main.csv').then(data => {
 //   console.log(data);
 // });
